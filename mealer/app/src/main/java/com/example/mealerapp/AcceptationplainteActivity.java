94b9d673efdeba9cedc1ea00091009d8e3e7c691
aichalf <113554602+aichalf@users.noreply.github.com>
2022-11-02 13:42:57 -0700

    Admin mise a jour

---
+++ mealer/app/src/main/java/com/example/mealerapp/AcceptationplainteActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptationplainteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acceptationplainte);
+    }
+}
---
+++ mealer/app/src/main/java/com/example/mealerapp/PlainteActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlainteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plainte);
+    }
+}
