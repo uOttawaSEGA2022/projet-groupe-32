dc4f07bdef144520e086f05e993577c3dc1b6290
amin-nna <aaminnnaa@gmail.com>
2022-11-04 18:22:35 -0400

    Plaintes

--- mealer/app/src/main/java/com/example/mealerapp/Plaintes.java
+++ mealer/app/src/main/java/com/example/mealerapp/Plaintes.java
@@ -5,7 +5,24 @@ public class Plaintes {
     String idClient;
     String idCuisinier;
     String datePlainte;
-    String idDemande;
-    String Etat;
+    boolean plainteTraitee=false;//Par défaut toutes les plaintes sont non résolues
+
+    public Plaintes(String titre_Plainte, String id_Client, String id_Cuisinier,String date_Plainte){
+        //On appelle le constructeur de la classe parent User
+        titrePlainte = titre_Plainte;
+        idClient = id_Client;
+        idCuisinier = id_Cuisinier;
+        datePlainte = date_Plainte;//Touver comment stocker une date
+    }
+
+    public boolean getPlainteTraitee(){
+        return plainteTraitee;
+    }
+
+    public void setPlainteTraitee(){
+        plainteTraitee = true;
+    }
+
+
 
 }
