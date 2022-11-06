86c095196c590acac9fb7aef1eef5408cfb0c70d
aichalf <113554602+aichalf@users.noreply.github.com>
2022-11-05 23:35:51 -0700

    admin

---
+++ mealer/app/src/main/java/com/example/mealerapp/SuspendrePlainte.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuspendrePlainte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suspendre_plainte);
+    }
+}
