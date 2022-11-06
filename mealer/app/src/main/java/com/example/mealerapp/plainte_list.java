939864ae78f74026419f788fab7d19952a5323f7
aichalf <113554602+aichalf@users.noreply.github.com>
2022-11-06 11:10:16 -0800

    plainte liste

---
+++ mealer/app/src/main/java/com/example/mealerapp/plainte_list.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plainte_list);
+    }
+}
