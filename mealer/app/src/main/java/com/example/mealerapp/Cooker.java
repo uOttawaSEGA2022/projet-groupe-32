28152051efcc03dd4da7f281a6a820c13cec7d97
amin-nna <aaminnnaa@gmail.com>
2022-11-05 22:46:16 -0400

    Plaintes

--- mealer/app/src/main/java/com/example/mealerapp/Cooker.java
+++ mealer/app/src/main/java/com/example/mealerapp/Cooker.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Cooker extends User{
     String description;
     String typeUser="Cooker";
-    List<Plaintes> listePlaintes = new ArrayList<Plaintes>();
+    List<Plainte> listePlaintes = new ArrayList<Plainte>();
 
     //Constructeur
 
