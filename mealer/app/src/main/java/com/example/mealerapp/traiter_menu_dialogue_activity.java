93b169712e9a65b02da2bc8d2fa8fb0f53d7ffe5
aichalf <113554602+aichalf@users.noreply.github.com>
2022-11-09 12:45:01 -0800

    Repas

--- mealer/app/src/main/java/com/example/mealerapp/traiter_menu_dialogue_activity.java
+++ mealer/app/src/main/java/com/example/mealerapp/traiter_menu_dialogue_activity.java
@@ -5,12 +5,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -23,7 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepasMain extends AppCompatActivity {
+public class traiter_menu_dialogue_activity extends AppCompatActivity {
 
 
     ListView listViewRepas;
@@ -66,7 +64,7 @@ public class RepasMain extends AppCompatActivity {
                     Repas repa = data.getValue(Repas.class) ;
                     repas.add(repa) ; }
 
-                RepasListe repasAdapter = new RepasListe(RepasMain.this, repas) ;
+                RepasListe repasAdapter = new RepasListe(traiter_menu_dialogue_activity.this, repas) ;
                 listViewRepas.setAdapter(repasAdapter) ;
             }
 
