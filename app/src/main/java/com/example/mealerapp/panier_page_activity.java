ded79754e2715d7dc87f115d1fa824b4566d0d9f
aichalf <113554602+aichalf@users.noreply.github.com>
2022-11-23 22:58:23 -0800

    client panel

---
+++ app/src/main/java/com/example/mealerapp/panier_page_activity.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class panier_page_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.panier_page);
+    }
+}
