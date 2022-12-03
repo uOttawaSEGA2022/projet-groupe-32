44e4d9c1fdaff63d4c7ad7511649665ce1a3b702
amin-nna <aaminnnaa@gmail.com>
2022-12-03 17:24:09 -0500

    Mise à jour Main Activity

--- app/src/main/java/com/example/mealerapp/MainActivity.java
+++ app/src/main/java/com/example/mealerapp/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 //
-
+        /*
           Repas repas1 = new Repas("test1", "test1", "test1", "test1", 10, "FeEbt2nOVDfgyq4Og8Y0bmwnINt1");
           Repas repas2 = new Repas("test2", "test2", "test2", "test2", 20, "FeEbt2nOVDfgyq4Og8Y0bmwnINt1");
           Repas repas3 = new Repas("test3", "test3", "test3", "test3", 30, "FeEbt2nOVDfgyq4Og8Y0bmwnINt1");
@@ -92,6 +92,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         demande3.addDemandeDatabase();
         Demande demande4 = new Demande("XVOKNWn03ZeviDgz8lFcGMaSeLz1", repas4);
         demande4.addDemandeDatabase();
+
+         */
 
 
 
